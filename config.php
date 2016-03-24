abae017cf12c85180cc389d3a12a0673ce754e35
Anna-Sagaydack <annasagaydack@gmail.com>
2016-03-24 19:12:52 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,6 +1,6 @@
 <?php
-$db_name = 'todobase';
-$db_user = 'mysql';
-$db_pass = 'mysql';
+$db_name = 'todolist_adr_com_ua';
+$db_user = 'anna90as';
+$db_pass = 'aaaaa1990';
 $db_host = 'localhost';
 $db = 'users';
